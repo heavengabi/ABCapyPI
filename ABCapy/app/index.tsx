5c4b721e7a47c9289822aec56049911a68873182
Gabi <heavenxg89@gmail.com>
2026-07-06 15:25:05 -0300

    feat:adiciona tela de jogos

--- ABCapy/app/index.tsx
+++ ABCapy/app/index.tsx
@@ -1,5 +1,15 @@
-import GamePages from "../app/gamePages";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  return <GamePages />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
 }
