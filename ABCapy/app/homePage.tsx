2d42008bfbb38d7899fd15d2b6d3a4f840197db5
Gabi <heavenxg89@gmail.com>
2026-07-19 20:52:25 -0300

    feat:adiciona a home page

--- ABCapy/app/homePage.tsx
+++ ABCapy/app/homePage.tsx
@@ -8,23 +8,67 @@ import {
   StyleSheet,
   ImageBackground,
 } from "react-native";
-
+import HomeCard from "@/src/components/homeComponents/HomeCard";
 import capyHome from "../src/assets/images/homeImages/capyHome.png";
 import gradiente from "../src/assets/images/homeImages/gradiente.png";
+import { router } from "expo-router";
+import speechBubble from "../src/assets/images/homeImages/speechBubble.png"
+import book from "../src/assets/images/homeImages/book.png"
+import estrela from "../src/assets/images/homeImages/estrela.png"
+import perfil from "../src/assets/images/homeImages/perfil.png"
+import globo from "../src/assets/images/homeImages/globo.png"
+import castelo from "../src/assets/images/homeImages/castelo.png"
+import menu from "../src/assets/images/homeImages/menu.png"
 const HomePage = () => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor:"#85ccffc9" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#85ccffc9" }}>
       <ImageBackground source={gradiente} style={styles.gradiente}>
         <Text style={styles.texto}>Olá!</Text>
         <Image source={capyHome} style={styles.capy} />
+
+        <Pressable style={styles.menuButton} onPress={() => { }}>
+          <Image source={menu} style={styles.menuIcon} />
+        </Pressable>
       </ImageBackground>
 
-      <View style = {styles.containerCards}>
-        <Text>O que vamos fazer?</Text>
-
+      <View style={styles.containerCards}>
+        <Text style={styles.texto2}>O que vamos fazer?</Text>
+        <HomeCard
+          title="Comunicação"
+          text="monte frases e se comunique"
+          image={speechBubble}
+          onPress={() => router.push("/gamePages")}
+        />
+        <HomeCard
+          title="Jogos"
+          text="aprenda brincando"
+          image={estrela}
+          onPress={() => router.push("/gamePages")}
+        />
+        <HomeCard
+          title="Histórias"
+          text="Explore novas Histórias"
+          image={book}
+          onPress={() => router.push("/gamePages")}
+        />
       </View>
 
-      <View style= {styles.footerBar}></View>
+      <View style={styles.footerBar}>
+        <View style={styles.footerBar}>
+          <Pressable style={styles.footerButton} onPress={() => { }}>
+            <Image source={globo} style={styles.icon} />
+          </Pressable>
+
+          <Pressable style={styles.footerButton} onPress={() => { }}>
+            <Image source={castelo} style={styles.icon} />
+          </Pressable>
+
+          <Pressable style={styles.footerButton} onPress={() => { }}>
+            <Image source={perfil} style={styles.icon} />
+          </Pressable>
+        </View>
+
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,46 +76,82 @@ const HomePage = () => {
 export default HomePage;
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    alignItems: "center",
-  }, 
-  containerCards:{
-    width: 393,
-    height:534,
-    backgroundColor:"white",
-    bottom: 20,
-    borderRadius:20,
-    borderColor:"#CDE9FF",
-    borderWidth:6
-
-
-  },
-  footerBar:{
-
+    backgroundColor: "#85ccffc9",
   },
 
   gradiente: {
-    width: 395,
-    height: 300,
+    width: "100%",
+    height: 290,
     justifyContent: "center",
     alignItems: "center",
   },
 
   texto: {
-    color: "#fff",
-    fontSize: 24,
+    color: "#297AB8",
+    fontSize: 22,
     fontWeight: "bold",
     position: "absolute",
     top: 50,
   },
 
   capy: {
-    width: 273,
-    height: 160,
+    width: 260,
+    height: 170,
+    resizeMode: "contain",
     position: "absolute",
-    bottom: 30,
+    bottom: 20,
   },
 
+  containerCards: {
+    flex: 1,
+    marginTop: -20,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderColor: "#CDE9FF",
+    borderWidth: 5,
+    alignItems: "center",
+    paddingTop: 18,
+  },
+
+  texto2: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#6ABFEF",
+    marginBottom: 15,
+  },
+
+  footerBar: {
+    height: 70,
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    gap: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  footerButton: {
+    padding: 10,
+  },
+
+  icon: {
+    width: 28,
+    height: 28,
+  },
+
+  menuButton: {
+    position: "absolute",
+    top: 15,
+    left: 15,
+    zIndex: 10,
+  },
+
+  menuIcon: {
+    width: 31,
+    height: 31,
+    resizeMode: "contain",
+  }
 });
+
--- ABCapy/src/components/homeComponents/HomeCard.tsx
+++ ABCapy/src/components/homeComponents/HomeCard.tsx
@@ -1,53 +1,75 @@
-// import { StyleSheet, Text, View, Image, Pressable } from "react-native";
-// import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Pressable,
+    ImageSourcePropType,
+} from "react-native";
+import React from "react";
 
-// type Props = {
-//   title: string;
-//   text: string;
-//   onPress: () => void;
-//   image: ImageSourcePropType;
-// };
+type Props = {
+    title: string;
+    text: string;
+    onPress: () => void;
+    image: ImageSourcePropType;
+};
 
-// const HomeCard = ({ title, text, onPress, image }: Props) => {
-//   return (
-//     <View>
-//       <View style={styles.cardBase}>
-//         <Pressable onPress={onPress} style={styles.btnStyle}>
-//           <Text style={styles.textStyle}>{text}</Text>
+const HomeCard = ({ title, text, onPress, image }: Props) => {
+    return (
+        <View >
+            <Pressable onPress={onPress} style={styles.btnStyle}>
+                <Image source={image} style={styles.image} />
 
-//           <View>
-//             <Image source={image} />
-//           </View>
-          
-//         </Pressable>
-//       </View>
-//     </View>
-//   );
-// };
+                <View style={styles.textContainer}>
+                    <Text style={styles.titleStyle}>{title}</Text>
+                    <Text style={styles.textStyle}>{text}</Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+};
 
-// export default HomeCard;
+export default HomeCard;
 
-// const styles = StyleSheet.create({
-//   btnStyle: {
-//     width: 295,
-//     height: 135,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "white",
-//     marginTop: 20,
-//     borderRadius: 15,
-//   },
-//   cardBase: {
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   textStyle: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//     textTransform: "uppercase",
-//     color: "#297AB8",
-//   },
+const styles = StyleSheet.create({
+    btnStyle: {
+        width: 340,
+        height: 90,
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#FFF",
+        marginTop: 15,
+        borderRadius: 15,
+        borderColor: "#93CCF7",
+        borderWidth: 5,
+        paddingHorizontal: 15,
+        elevation: 4,
+    },
 
+    image: {
+        width: 50,
+        height: 50,
+        resizeMode: "contain",
+    },
 
+    textContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
 
-// });
+    titleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#297AB8",
+        textAlign: "center",
+    },
+
+    textStyle: {
+        fontSize: 14,
+        color: "#000000",
+        textAlign: "center",
+    },
+
+})
