6f14472fcc460f757120b28d4b337b4d293f8014
Gabi <heavenxg89@gmail.com>
2026-08-03 16:54:45 -0300

    feat:adiciona arquivos

---
+++ ABCapy/app/Stories.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Footer from '@/src/components/Footer/Footer'
+
+const Stories = () => {
+  return (
+    <Footer />
+  )
+}
+
+export default Stories
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
